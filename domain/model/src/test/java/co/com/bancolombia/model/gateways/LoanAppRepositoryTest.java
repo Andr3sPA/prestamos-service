3eb5054bb7bcf2b8c5b672c62df1437ebb63919c
Andrés <andpena@bancolombia.com.co>
2025-08-28 09:19:10 -0500

    Update LoanAppRepositoryTest.java

--- domain/model/src/test/java/co/com/bancolombia/model/gateways/LoanAppRepositoryTest.java
+++ domain/model/src/test/java/co/com/bancolombia/model/gateways/LoanAppRepositoryTest.java
@@ -1,14 +1,20 @@
 package co.com.bancolombia.model.gateways;
 
 import co.com.bancolombia.model.LoanApplication;
-import co.com.bancolombia.dto.LoanApplicationRequest;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class LoanAppRepositoryTest {
+
     @Test
     void testRegisterMethodSignature() {
         LoanAppRepository repo = (loanApp) -> Mono.just(new LoanApplication());
-        LoanApplicationRequest request = new LoanApplicationRequest();
-        Mono<LoanApplication> result = repo.register(request);
+
+        LoanApplication loanApplication = new LoanApplication(); // ya no LoanApplicationRequest
+        Mono<LoanApplication> result = repo.register(loanApplication);
+
         assertNotNull(result);
         assertNotNull(result.block());
     }
